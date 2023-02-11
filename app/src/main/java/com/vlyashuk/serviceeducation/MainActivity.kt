3fa1a63c894f75506570d63929a3f15af663d3d6
Владимир Ляшук <89472439+wowus94@users.noreply.github.com>
2023-02-11 10:27:52 +0300

    Initial commit

---
+++ app/src/main/java/com/vlyashuk/serviceeducation/MainActivity.kt
@@ -0,0 +1,17 @@
+package com.vlyashuk.serviceeducation
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import com.vlyashuk.serviceeducation.databinding.ActivityMainBinding
+
+class MainActivity : AppCompatActivity() {
+
+    private val binding by lazy {
+        ActivityMainBinding.inflate(layoutInflater)
+    }
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(binding.root)
+    }
+}
