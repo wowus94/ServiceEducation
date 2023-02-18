986e22daf3a577dead6ff6e105615d4663a4b4ed
Владимир Ляшук <89472439+wowus94@users.noreply.github.com>
2023-02-18 15:21:37 +0300

    JobScheduler. Method enqueue

--- app/src/main/java/com/vlyashuk/serviceeducation/MainActivity.kt
+++ app/src/main/java/com/vlyashuk/serviceeducation/MainActivity.kt
@@ -2,7 +2,9 @@ package com.vlyashuk.serviceeducation
 
 import android.app.job.JobInfo
 import android.app.job.JobScheduler
+import android.app.job.JobWorkItem
 import android.content.ComponentName
+import android.os.Build
 import android.os.Bundle
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.content.ContextCompat
@@ -13,6 +15,8 @@ class MainActivity : AppCompatActivity() {
     private val binding by lazy {
         ActivityMainBinding.inflate(layoutInflater)
     }
+
+    private var page = 0
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -41,7 +45,12 @@ class MainActivity : AppCompatActivity() {
                 .build()
 
             val jobScheduler = getSystemService(JOB_SCHEDULER_SERVICE) as JobScheduler
-            jobScheduler.schedule(jobInfo)
+
+
+            if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.O) {
+                val intent = MyJobService.newIntent(page++)
+                jobScheduler.enqueue(jobInfo, JobWorkItem(intent))
+            }
         }
     }
 }
--- app/src/main/java/com/vlyashuk/serviceeducation/MyJobService.kt
+++ app/src/main/java/com/vlyashuk/serviceeducation/MyJobService.kt
@@ -2,6 +2,8 @@ package com.vlyashuk.serviceeducation
 
 import android.app.job.JobParameters
 import android.app.job.JobService
+import android.content.Intent
+import android.os.Build
 import android.util.Log
 import kotlinx.coroutines.*
 
@@ -17,12 +19,20 @@ class MyJobService : JobService() {
 
     override fun onStartJob(params: JobParameters?): Boolean {
         log("onStartJob")
-        coroutineScope.launch {
-            for (i in 0 until 100) {
-                delay(1000)
-                log("Timer $i")
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.O) {
+            coroutineScope.launch {
+                var workItem = params?.dequeueWork()
+                while (workItem != null) {
+                    val page = workItem.intent.getIntExtra(PAGE, 0)
+                    for (i in 0 until 5) {
+                        delay(1000)
+                        log("Timer $i $page")
+                    }
+                    params?.completeWork(workItem)
+                    workItem = params?.dequeueWork()
+                }
+                jobFinished(params, false)
             }
-            jobFinished(params, true)
         }
         return true
     }
@@ -42,8 +52,14 @@ class MyJobService : JobService() {
         Log.d("SERVICE_TAG", "MyJobService: $message")
     }
 
-    companion object{
+    companion object {
         const val JOB_ID = 123
-    }
+        private const val PAGE = "page"
 
+        fun newIntent(page: Int): Intent {
+            return Intent().apply {
+                putExtra(PAGE, page)
+            }
+        }
+    }
 }
